1721b7a27badd68e3da267cc6dcf83c21b0b12fa
Leandro Brangi <leandro.brangi@gmail.com>
2023-04-29 17:25:40 -0300

    Acondicinamiento de repo

---
+++ client/views/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home/Home";
+import Landing from "./Landing/Landing";
+import Create from "./Create/Create";
+import Detail from "./Detail/Detail"
+
+
+export { Home, Landing, Create , Detail };
