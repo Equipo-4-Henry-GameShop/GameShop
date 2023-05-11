aecac2c193442586e4007a8ea90ad1d550a490b3
Leandro Brangi <leandro.brangi@gmail.com>
2023-05-11 18:03:36 -0300

    update

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import videogamesReducer from "./videogamesSlice";
-import userReducer from "./usersSlices"
-export default configureStore({
-    reducer:{
-        videogamesState: videogamesReducer,
-        usersState: userReducer
-    }
-})
