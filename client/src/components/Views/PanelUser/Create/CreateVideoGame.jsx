49a9dcd962635ce17fec2482e0c8394d5056af15
Leandro Brangi <leandro.brangi@gmail.com>
2023-05-15 09:44:53 -0300

    actualización o ordemamiento de ficheros

--- client/src/components/Views/PanelUser/Create/CreateVideoGame.jsx
+++ client/src/components/Views/PanelUser/Create/CreateVideoGame.jsx
@@ -9,12 +9,9 @@ import {
   FlatList,
 } from "react-native";
 
-import {
+import { SelectList } from "react-native-dropdown-select-list";
 
-  allGenres,
-  allPlatforms,
-
-} from "./components/dataFilteredgames";
+import { allGenres, allPlatforms } from "./components/dataFilteredgames";
 import { useState, useRef, useEffect } from "react";
 import {
   color_azul,
@@ -22,8 +19,12 @@ import {
   color_negro,
 } from "../../../../constants/Colors";
 
+import { ImagePickerExample } from "../../../helpers/imagePicker";
+
 const CreateVideogame = () => {
   const inputRef = useRef();
+
+  
   const [stackData, setStackData] = useState({
     platform: allPlatforms,
     image: "",
@@ -39,19 +40,32 @@ const CreateVideogame = () => {
     genres: [],
     screnShots: "",
     releaseDate: "",
-    tags: "",
+    tags: [],
   });
+
+  // useEffect(() => {
+  //   pushItemGenres()
+  //   removeItemGenres()
+    
+  // }, [stackData.platform, newVideoGame.platform])
+  
+  console.log(newVideoGame)
+  console.log(stackData)
+    
 
   const pushItemGenres = (value) => {
     setNewVideoGame((prevState) => ({
       ...prevState,
       genres: [...prevState.genres, value],
-    }));
+
+    })) ;
+
 
     setStackData((prevState) => ({
       ...prevState,
       genres: prevState.genres.filter((p) => p !== value),
     }));
+
   };
 
   const removeItemGenres = (value) => {
@@ -126,12 +140,11 @@ const CreateVideogame = () => {
   return (
     <View style={styles.container}>
       <ScrollView>
-         
-
+        {/* <ImagePickerExample/> */}
         <View>
           <Text style={styles.title}>Name: </Text>
           <TextInput
-          style={styles.input}
+            style={styles.input}
             placeholder="Enter the name of the game"
             value={newVideoGame.name}
             keyboardAppearance="light"
@@ -184,68 +197,59 @@ const CreateVideogame = () => {
               onChangeText={(text) => handleInputChange("name", text)}
             />
           </View>
+
           <Text style={styles.title}>Add Genre</Text>
-        <View style={styles.container}>
-          <FlatList
-            data={stackData.genres}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => pushItemGenres(item)}
-                style={styles.miniButton}
-              >
-                <Text style={styles.buttonText}>{item}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        </View>
-          <Text style={styles.title}>Quitar</Text>
-        <View style={styles.boxButtons}>
-          <FlatList
-            data={newVideoGame.genres}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => removeItemGenres(item)}
-                style={styles.miniButton}
-              >
-                <Text style={styles.buttonText} >{item}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        </View>
+          <View>
+            <SelectList
+              placeholder="Add genre"
+              setSelected={(val) => pushItemGenres(val)}
+              data={stackData.genres}
+              search={false}
+            />
+          </View>
+          <View>
+            <SelectList
+              placeholder="Remove genres"
+              setSelected={(val) => removeItemGenres(val)}
+              data={newVideoGame.genres}
+              search={false}
+            />
+          </View>
 
-
-        <Text style={styles.title}>Add Platform</Text>
-        <View style={styles.boxButtons}>
-          <FlatList
-            data={stackData.platform}
-            renderItem={({ item }) => (
-              <View style={styles.boxButtons}>
-              <TouchableOpacity
-                onPress={() => pushItemPlatform(item)}
-                style={styles.miniButton}
-              >
-                <Text style={styles.buttonText}>{item}</Text>
-              </TouchableOpacity>
-              </View>
-            )}
-          />
-        </View>
-          <Text style={styles.title}>Quitar</Text>
-        <View >
-          <FlatList
-            data={newVideoGame.platform}
-            renderItem={({ item }) => (
-              <View style={styles.boxButtons}>
-              <Button
-                onPress={() => removeItemPlatform(item)}
-                style={styles.miniButton}
-              >
-                <Text style={styles.buttonText} >{item}</Text>
-              </Button>
-              </View>
-            )}
-          />
-        </View>
+          <Text style={styles.title}>Add Platform</Text>
+          <View>
+            <SelectList
+              placeholder="Add platform"
+              setSelected={(val) => pushItemPlatform(val)}
+              data={stackData.platform}
+              search={false}
+            />
+          </View>
+          <View>
+            <SelectList
+              placeholder= "Remove platforms"
+              setSelected={(val) => removeItemPlatform(val)}
+              data={newVideoGame.platform}
+              search={false}
+            />
+          </View>
+          <Text style={styles.title}>Add Tags</Text>
+          <View>
+            <SelectList
+              placeholder="Add tag"
+              setSelected={(val) => pushItemPlatform(val)}
+              data={stackData.platform}
+              search={false}
+            />
+          </View>
+          <View>
+            <SelectList
+              placeholder= "Remove tag"
+              setSelected={(val) => removeItemPlatform(val)}
+              data={newVideoGame.platform}
+              search={false}
+            />
+          </View>
           <View>
             <Button title="Submit"></Button>
           </View>
@@ -257,36 +261,31 @@ const CreateVideogame = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection:"row",
-    // paddingTop: Constants.statusBarHeight + 200,
-    backgroundColor: "#ecf0f1",
-    padding: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
   },
+
   title: {
     margin: 24,
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
   },
-  
-  boxButtons:{
 
-    flexDirection:"row",
-    justifyContent: 'space-around',
-    alignItems: 'center',
+  boxButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     padding: 10,
-
-
-    
   },
   miniButton: {
-    
-    margin:"2%",
-    width:"20%",
-    padding:0,
+    margin: "2%",
+    width: "20%",
+    padding: 0,
     backgroundColor: color_azul,
     borderRadius: 8,
-    color: color_blanco ,
+    color: color_blanco,
   },
   error: {
     margin: 8,
@@ -306,10 +305,10 @@ const styles = StyleSheet.create({
     textAlign: "center",
     padding: 10,
     fontSize: 10,
-    
+
     fontWeight: "bold",
-    color: color_blanco
-  }
+    color: color_blanco,
+  },
 });
 
-export default CreateVideogame
+export default CreateVideogame;
